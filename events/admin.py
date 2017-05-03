bb4137ad0450533d742dea78052b139e4387604f
Maddie Gordon <maddie@Madelines-MacBook-Pro-2.local>
2017-05-02 23:09:22 -0700

    authenticated user

--- events/admin.py
+++ events/admin.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+from events.models import Event
 
 from django.contrib import admin
 
-# Register your models here.
+admin.site.register(Event)
