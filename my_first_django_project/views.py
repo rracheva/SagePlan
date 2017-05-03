f4065cfe6f1961c1827404692391378d818916f4
Maddie Gordon <maddie@Madelines-MacBook-Pro-2.local>
2017-05-02 22:52:44 -0700

    authenticated user

--- my_first_django_project/views.py
+++ my_first_django_project/views.py
@@ -9,30 +9,30 @@ from django.template import loader
 # Create your views here.
 
 def home(request):
-	#return HttpResponse("<h1>Index Page</h1>")
+    #return HttpResponse("<h1>Index Page</h1>")
     if request.user.is_authenticated:
-	   hosted_events = Event.objects.filter(creator=request.user)
-	   invited_to = Invite.objects.filter(invitee=request.user)
-	   invited_to_ids = invited_to.values_list('event_id', flat=True)
-	   invited_events = []
-	   for event in Event.objects.all():
-		    print event.event_id
-		    if event.event_id in invited_to_ids:
-			   invited_events.append(event)
+       hosted_events = Event.objects.filter(creator=request.user)
+       invited_to = Invite.objects.filter(invitee=request.user)
+       invited_to_ids = invited_to.values_list('event_id', flat=True)
+       invited_events = []
+       for event in Event.objects.all():
+            print event.event_id
+            if event.event_id in invited_to_ids:
+               invited_events.append(event)
 
 
-	# html = ''
-	# for event in all_events:
-	# 	url = '/events/' + str(event.event_id) + '/'
-	# 	html += '<a href="' + url + '">' + event.title + '</a><br>'
-	# return HttpResponse(html)
-    	template = loader.get_template('home.html')
-    	context = {
-    		'all_events' : Event.objects.all(),
-    		'hosted_events' : hosted_events,
-    		'invited_events' : invited_events, 
-    	}
-	return HttpResponse(template.render(context))
+    # html = ''
+    # for event in all_events:
+    #   url = '/events/' + str(event.event_id) + '/'
+    #   html += '<a href="' + url + '">' + event.title + '</a><br>'
+    # return HttpResponse(html)
+        template = loader.get_template('home.html')
+        context = {
+            'all_events' : Event.objects.all(),
+            'hosted_events' : hosted_events,
+            'invited_events' : invited_events, 
+        }
+    return HttpResponse(template.render(context))
 
 
 
